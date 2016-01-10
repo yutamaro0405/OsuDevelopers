590149b45b9b0c86cbdc8d9916e5d625e08f1679
chillyPleiades <amkt.moki@gmail.com>
2016-01-10 19:36:28 +0900

    車基本クラスの修正と7人乗り車の追加

--- SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/Car_Wish7.java
+++ SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/Car_Wish7.java
@@ -1,25 +1,27 @@
 package com.osudevelopers.seatallocation;
 
 /**
- * 5人乗りセダンオブジェクトクラス
+ * 7人乗りWishオブジェクトクラス
  * Created by chilly on 2016/01/10.
  */
-public class Car_Sedan5 extends CarCar{
+public class Car_Wish7 extends CarCar{
     /**
-     * 5人乗りセダンオブジェクトクラス
+     * 7人乗りWishコンストラクタ
      *
      *   1       0
      *   4   3   2
+     *   6       5
      *
      * @throws CarException
      */
-    Car_Sedan5() throws CarException {
-        super("5人乗りセダン",5,3,2);
+    Car_Wish7() throws CarException {
+        super("7人乗りウィッシュ",7,3,3);
         addCoordinate(0, 2, 0);
         addCoordinate(1, 0, 0);
         addCoordinate(2, 2, 1);
         addCoordinate(3, 1, 1);
         addCoordinate(4, 0, 1);
+        addCoordinate(5, 2, 2);
+        addCoordinate(6, 0, 2);
     }
-
 }
