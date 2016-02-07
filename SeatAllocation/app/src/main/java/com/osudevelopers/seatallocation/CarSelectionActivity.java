a21e7a2151fef8bd9b79dc46fd8cc26c4c3559a6
yutamaro0405 <yutamaro0405@gmail.com>
2016-02-07 20:01:30 +0900

    commit

--- SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/CarSelectionActivity.java
+++ SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/CarSelectionActivity.java
@@ -2,13 +2,19 @@ package com.osudevelopers.seatallocation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Spinner;
 
-public class CarSelectionActivity extends AppCompatActivity {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class CarSelectionActivity extends AppCompatActivity implements Serializable{
+    public final static String EXTRA_LISTCAR = "com.osudevelopers.seatallocation.LISTCAR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +25,23 @@ public class CarSelectionActivity extends AppCompatActivity {
 
     }
 
-    public void goCarPeoples(View view){
+    public void goCarPeoples(View view) throws CarException{
+        ArrayList<CarCar> listCar = new ArrayList<CarCar>();
+        Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
+        int num5 = Integer.parseInt((String)spinner5.getSelectedItem());
+        listCar.add(new Car_Sedan5());
+        for(int i=0; i < num5; i++){
+            listCar.add(new Car_Sedan5());
+        }
+        Spinner spinner7 = (Spinner) findViewById(R.id.spinner7);
+        int num7 = Integer.parseInt((String)spinner7.getSelectedItem());
+        for(int i=0; i < num7; i++){
+            listCar.add(new Car_Wish7());
+        }
+
+
         Intent intent = new Intent(this, CarPeoplesActivity.class);
+        intent.putExtra(EXTRA_LISTCAR, listCar);
         startActivity(intent);
     }
 
