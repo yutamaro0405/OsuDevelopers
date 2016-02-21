c28f5fa2e2e2725ff39f40f2f88e4fd376684ba0
yutamaro0405 <yutamaro0405@gmail.com>
2016-02-21 17:41:08 +0900

    public Context appContext = getApplicationContext();

--- SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/ResultActivity.java
+++ SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/ResultActivity.java
@@ -1,7 +1,5 @@
 package com.osudevelopers.seatallocation;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,18 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    public Context appContext = getApplicationContext();
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        Intent intent = new Intent(this, CarSelectionActivity.class);
-        startActivity(intent);
 
     }
 
