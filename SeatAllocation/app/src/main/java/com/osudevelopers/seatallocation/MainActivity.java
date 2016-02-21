c56af3f871619a5e21d94dde63247b242bff0bca
chillyPleiades <amkt.moki@gmail.com>
2016-02-21 19:30:24 +0900

    テスト完了

--- SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/MainActivity.java
+++ SeatAllocation/app/src/main/java/com/osudevelopers/seatallocation/MainActivity.java
@@ -1,16 +1,11 @@
 package com.osudevelopers.seatallocation;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    public Context appContext = getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
